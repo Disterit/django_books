4d3a9b2fa92397d859c82655fd1a9ed85ce5af3d
Dister <71532605+Disterit@users.noreply.github.com>
2023-06-14 16:17:21 +0700

    add main create django, and docker create superuser and user

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#local
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser' #new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
