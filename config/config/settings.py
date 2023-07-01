60fc885ac8c27e1be4d40afb4c35c8c72184c6e4
Dister <71532605+Disterit@users.noreply.github.com>
2023-07-01 13:41:42 +0700

    add main create django, and docker create superuser and user create test for pages create page with registration, tests commit crisp forms for nice view create new sign up system with test

--- config/config/settings.py
+++ config/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,10 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--)v=4z1ey5)ne5vfxoft9a01x8)ne)o)2#82lm48z^myr5s**('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+
+
+env = Env()
+env.read_env()
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+DEBUG = env.bool("DJANGO_DEBUG")
 
 # Application definition
 
@@ -107,14 +113,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
+    "default": env.dj_db_url("DATABASE_URL",
+                             default = "postgres://postgres@db/postgres")
 }
 
 
