4c766934d1df7a889016d181517a874174014984
Dister <71532605+Disterit@users.noreply.github.com>
2023-07-05 18:48:31 +0700

    add main create django, and docker create superuser and user create test for pages create page with registration, tests commit crisp forms for nice view create new sign up system with test create email control create url for books

--- config/books/tests.py
+++ config/books/tests.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 
 class BookTest(TestCase):
@@ -9,6 +10,16 @@ class BookTest(TestCase):
             title='Harry Potter',
             author='JK Rowling',
             price='25.00'
+        )
+        self.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@mail.ru',
+            password='testpass123'
+        )
+        self.review = Review.objects.create(
+            book=self.book,
+            author=self.user,
+            review='An excellent review'
         )
 
     def test_book_listing(self):
@@ -22,10 +33,11 @@ class BookTest(TestCase):
         self.assertContains(response, 'Harry Potter')
         self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def tets_book_detail_view(self):
+    def test_book_detail_view(self):
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get('books/12345')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'books/book_detail.html')
